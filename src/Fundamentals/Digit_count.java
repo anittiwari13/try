43795dc35970c81117717ce900f623b0dde436a1
ACER <tiwarianit111@gmail.com>
2022-01-10 16:09:27 +0530

    hhh

---
+++ src/Fundamentals/Digit_count.java
@@ -0,0 +1,7 @@
+package Fundamentals;
+
+public class Digit_count {
+    public static void main(String[] args) {
+        System.out.println("Hye i m difgit count");
+    }
+}
---
+++ src/Fundamentals/Reverse_a_Number.java
@@ -0,0 +1,7 @@
+package Fundamentals;
+
+public class Reverse_a_Number {
+    public static void main(String[] args) {
+        System.out.println("Hey I am Reverse a number");
+    }
+}
